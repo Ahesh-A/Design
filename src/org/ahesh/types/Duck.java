b0fce364a4d24fcc73376cd8645651656b897f5f
Ahesh-A <aheshalagu@gmail.com>
2024-09-16 22:33:58 +0530

    added setFlyBehaviour in class Duck

--- src/org/ahesh/types/Duck.java
+++ src/org/ahesh/types/Duck.java
@@ -1,7 +1,12 @@
 package org.ahesh.types;
+import org.ahesh.types.DuckInterface.FlyBehaviour;
 
 public class Duck {
+
     FlyBehaviour flyBehaviour;
+    public Duck(FlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
+    }
     public void swim() {
         System.out.println("Im Generic duck swimming, quack quack...");
     }
@@ -10,4 +15,12 @@ public class Duck {
         System.out.println("Im generic duck, displaying myself");
     }
 
+    public void performFly() {
+        if(flyBehaviour != null) {
+            flyBehaviour.fly();
+        }
+    }
+    public void setFlyBehaviour(FlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
+    }
 }
