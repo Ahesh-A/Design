766ef947103cfb4ea64cf94686291db6392be542
Ahesh-A <aheshalagu@gmail.com>
2024-09-18 22:40:25 +0530

    implemented function display and update

--- src/org/ahesh/observer/classes/Display/StatisticsDisplay.java
+++ src/org/ahesh/observer/classes/Display/StatisticsDisplay.java
@@ -1,4 +1,13 @@
 package org.ahesh.observer.classes.Display;
 
-public class StatisticsDisplay {
+import org.ahesh.observer.types.Display;
+import org.ahesh.observer.types.Observer;
+
+public class StatisticsDisplay implements Display, Observer {
+    public void display() {
+
+    }
+    public void update() {
+
+    }
 }
