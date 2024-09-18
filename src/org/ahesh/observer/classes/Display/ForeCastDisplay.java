47989b086b6582da0268576bd4492bcef427a6b8
Ahesh-A <aheshalagu@gmail.com>
2024-09-18 22:39:32 +0530

    implemented function display and update

--- src/org/ahesh/observer/classes/Display/ForeCastDisplay.java
+++ src/org/ahesh/observer/classes/Display/ForeCastDisplay.java
@@ -1,4 +1,14 @@
 package org.ahesh.observer.classes.Display;
 
-public class ForeCastDisplay {
+import org.ahesh.observer.types.Display;
+import org.ahesh.observer.types.Observer;
+
+public class ForeCastDisplay implements Display, Observer{
+    public void display() {
+
+    }
+
+    public void update() {
+
+    }
 }
