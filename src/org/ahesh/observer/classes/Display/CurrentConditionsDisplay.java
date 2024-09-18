33793f0d6bf8a33e614db29349ee035d7baffdc8
Ahesh-A <aheshalagu@gmail.com>
2024-09-18 22:33:27 +0530

    added interface StatisticsDisplay

--- src/org/ahesh/observer/classes/Display/CurrentConditionsDisplay.java
+++ src/org/ahesh/observer/classes/Display/CurrentConditionsDisplay.java
@@ -1,10 +1,14 @@
 package org.ahesh.observer.classes.Display;
 
+import org.ahesh.observer.types.Display;
 import org.ahesh.observer.types.Observer;
 
-public class CurrentConditionsDisplay implements Observer{
+public class CurrentConditionsDisplay implements Observer, Display {
     public void update() {
 
     }
 
+    public void display(){
+
+    }
 }
---
+++ src/org/ahesh/observer/classes/Display/StatisticsDisplay.java
@@ -0,0 +1,4 @@
+package org.ahesh.observer.classes.Display;
+
+public interface StatisticsDisplay {
+}
