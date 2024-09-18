9cff9e8c6f5bcb0706277133ff7f7b9e7945ba33
Ahesh-A <aheshalagu@gmail.com>
2024-09-18 22:30:38 +0530

    added interface Display

---
+++ src/org/ahesh/observer/classes/Display/CurrentConditionsDisplay.java
@@ -0,0 +1,10 @@
+package org.ahesh.observer.classes.Display;
+
+import org.ahesh.observer.types.Observer;
+
+public class CurrentConditionsDisplay implements Observer{
+    public void update() {
+
+    }
+
+}
