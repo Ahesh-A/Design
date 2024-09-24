a04bbe14662eb99f00ead79c142ca2e0e083e1c5
Ahesh-A <aheshalagu@gmail.com>
2024-09-24 23:58:33 +0530

    added  class CondimentDecorator

---
+++ src/org/ahesh/decarator/condiments/Mocha.java
@@ -0,0 +1,10 @@
+package org.ahesh.decarator.condiments;
+import org.ahesh.decarator.CondimentDecorator;
+
+public class Mocha extends CondimentDecorator{
+
+    @Override
+    public double cost() {
+        return 0.15;
+    }
+}
