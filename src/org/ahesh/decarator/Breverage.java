db4cc80978f5d50db6484cb020bdd2ed8bbdd031
Ahesh-A <aheshalagu@gmail.com>
2024-09-24 23:42:55 +0530

    added function getDescription and cost

--- src/org/ahesh/decarator/Breverage.java
+++ src/org/ahesh/decarator/Breverage.java
@@ -1,5 +1,10 @@
 package org.ahesh.decarator;
 
-public abstract  class Breverage {
+public abstract class Breverage {
+    String description = "This is a beverage";
+    public String getDescription() {
+        return this.description;
+    }
 
+    public abstract double cost();
 }
