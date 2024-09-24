f468b08402dda925e949f431211fba393b9d477c
Ahesh-A <aheshalagu@gmail.com>
2024-09-24 23:56:21 +0530

    added class SteamedMilk

---
+++ src/org/ahesh/decarator/condiments/SteamedMilk.java
@@ -0,0 +1,10 @@
+package org.ahesh.decarator.condiments;
+
+import org.ahesh.decarator.CondimentDecorator;
+
+public class SteamedMilk extends CondimentDecorator {
+    @Override
+    public double cost() {
+        return 0.10;
+    }
+}
