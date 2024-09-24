6e84a9a440575e477efc11c2e2553635e56b562e
Ahesh-A <aheshalagu@gmail.com>
2024-09-24 23:44:43 +0530

    extended class DarkRoast from Breverage and added function cost

---
+++ src/org/ahesh/decarator/beverages/DarkRoast.java
@@ -0,0 +1,9 @@
+package org.ahesh.decarator.beverages;
+
+import org.ahesh.decarator.Breverage;
+
+public class DarkRoast extends Breverage {
+    public double cost() {
+        return 0.99;
+    }
+}
