9f5ae68ebd6368895a944477bdae979e79d1bf1e
Ahesh-A <aheshalagu@gmail.com>
2024-09-26 22:26:46 +0530

    added abstract function getDescription

--- src/org/ahesh/decarator/CondimentDecorator.java
+++ src/org/ahesh/decarator/CondimentDecorator.java
@@ -3,4 +3,5 @@ package org.ahesh.decarator;
 public abstract class CondimentDecorator extends Breverage{
     public Breverage breverage;
     public abstract double cost();
+    public abstract String getDescription();
 }
