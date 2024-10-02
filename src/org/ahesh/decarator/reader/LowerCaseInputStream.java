235a7feb2ca6bee2e62c2ec0e210b6681fbe04ce
Ahesh-A <aheshalagu@gmail.com>
2024-10-02 19:36:57 +0530

    overriden read for (byte[], int, int)

--- src/org/ahesh/decarator/reader/LowerCaseInputStream.java
+++ src/org/ahesh/decarator/reader/LowerCaseInputStream.java
@@ -13,7 +13,16 @@ public  class LowerCaseInputStream extends FilterInputStream {
 
     public int read() throws IOException {
         int c = in.read();
-
         return c == -1 ? c : (int)Character.toLowerCase((char)c);
+    }
+
+    public int read(byte[] b, int off, int len) throws IOException {
+        int res = in.read(b, off, len);
+
+        for(int i = off; i < off + res; i++) {
+            b[i] = (byte)Character.toLowerCase((char)b[i]);
+        }
+
+        return res;
     }
 }
