c917dcbdf7058bb9e8f1f233a718f44a6f794b99
Ahesh-A <aheshalagu@gmail.com>
2024-10-03 22:42:12 +0530

    modified cost function of Soy

--- src/org/ahesh/decarator/condiments/Soy.java
+++ src/org/ahesh/decarator/condiments/Soy.java
@@ -9,7 +9,22 @@ public class Soy extends CondimentDecorator {
     }
     @Override
     public double cost() {
-        return 0.15 + breverage.cost();
+        double cost = 0.15 + breverage.cost();
+        Size size = breverage.getSize();
+        if(size == Size.VENTI) {
+            cost += 0.1;
+        }
+
+        if(size == Size.GRANDE) {
+            cost += 0.15;
+        }
+
+        if(size == Size.TALL) {
+            cost += 0.2;
+        }
+
+        return cost;
+//        return 0.15 + breverage.cost();
     }
     @Override
     public String getDescription() {
