e5597a60230cb624092d41b51caa99302bd89751
Ahesh-A <aheshalagu@gmail.com>
2024-10-09 08:41:26 +0530

    added abstract function createPizza

--- src/org/ahesh/factory/Store.java
+++ src/org/ahesh/factory/Store.java
@@ -1,10 +1,12 @@
 package org.ahesh.factory;
 
+import org.ahesh.factory.Pizza.Pizza;
 import org.ahesh.factory.types.PizzaType;
 public abstract class Store {
-    public void orderPizza(PizzaType pizza) {
-
-
+    public void orderPizza(PizzaType pizzaType) {
+        Pizza pizza = createPizza();
     }
+
+    public abstract Pizza createPizza();
 
 }
