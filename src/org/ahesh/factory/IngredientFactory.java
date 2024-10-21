3643188c5a4ecd5fb927a21c9b7581291efb787b
Ahesh-A <aheshalagu@gmail.com>
2024-10-21 23:08:26 +0530

    added method signature createDough, createSauce, createCheese, createVeggies, createClams, createPepperoni

--- src/org/ahesh/factory/IngredientFactory.java
+++ src/org/ahesh/factory/IngredientFactory.java
@@ -1,4 +1,11 @@
 package org.ahesh.factory;
 
+import org.ahesh.factory.ingredients.*;
+
 public interface IngredientFactory {
+    void createDough();
+    void createSauce();
+    void createCheese();
+    void createClams();
+    void createPepperoni();
 }
