fca66a58870ba1d1a0c85bddfc0b8f03cf847dbb
Ahesh-A <aheshalagu@gmail.com>
2024-10-22 23:32:19 +0530

    added pizza type ClamPizza

--- src/org/ahesh/factory/Pizza/types/ClamPizza.java
+++ src/org/ahesh/factory/Pizza/types/ClamPizza.java
@@ -1,4 +1,21 @@
 package org.ahesh.factory.Pizza.types;
 
-public class ClamPizza {
+import org.ahesh.factory.Pizza.Pizza;
+import org.ahesh.factory.ingerdientfactory.PizzaIngredientFactory;
+
+public class ClamPizza extends Pizza {
+    PizzaIngredientFactory pizzaIngredientFactory;
+    public ClamPizza(PizzaIngredientFactory pizzaIngredientFactory) {
+        this.pizzaIngredientFactory = pizzaIngredientFactory;
+    }
+
+    @Override
+    public void prepare() {
+        dough = pizzaIngredientFactory.createDough();
+        sauce = pizzaIngredientFactory.createSauce();
+        cheese = pizzaIngredientFactory.createCheese();
+        veggies = pizzaIngredientFactory.createVeggies();
+        clams = pizzaIngredientFactory.createClams();
+        pepperoni = pizzaIngredientFactory.createPepperoni();
+    }
 }
