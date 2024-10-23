c83fe361c4266345e361247bb5bdd824030699d2
Ahesh-A <aheshalagu@gmail.com>
2024-10-23 22:52:09 +0530

    added class Singleton and method getInstance

--- src/org/ahesh/singleton/Singleton.java
+++ src/org/ahesh/singleton/Singleton.java
@@ -1,4 +1,12 @@
 package org.ahesh.singleton;
 
 public class Singleton {
+    static Singleton singleton;
+    private Singleton() {}
+    public static Singleton getInstance() {
+        if(singleton == null) {
+           singleton = new Singleton();
+        }
+        return singleton;
+    }
 }
