42d83abb07954cd5224c3ce16e98833f6f7c5f3e
Ahesh-A <aheshalagu@gmail.com>
2024-11-07 23:00:21 +0530

    added lightSetter and constructor

--- src/org/ahesh/command/commandlist/LightsOnCommand.java
+++ src/org/ahesh/command/commandlist/LightsOnCommand.java
@@ -1,10 +1,17 @@
 package org.ahesh.command.commandlist;
 
 import org.ahesh.command.Command;
-
+import org.ahesh.command.commandObjects.Light;
 public class LightsOnCommand implements Command {
+    Light light;
+    public LightsOnCommand(Light light) {
+        this.light = light;
+    }
 
+    public void lightSetter(Light light) {
+        this.light = light;
+    }
     public void execute() {
-
+        light.on();
     }
 }
